a66969f2c529356264d5372599e22f9e3f13045f
Elizabeth <elizaveta.chernosheikina@yandex.ru>
2023-04-29 21:20:09 +0300

    create singIn component and routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import { Route, Routes } from "react-router-dom";
 import SignUp from "./components/SignUp/SignUp";
+import SignIn from './components/SignIn/SignIn';
 
 function App() {
   return ( 
     <>
-      <SignUp />
+      <Routes>
+        <Route exact path="*" element={ <SignUp/> }/>
+        <Route exact path="/signup" element={ <SignUp/> }/>
+        <Route exact path="/signin" element={ <SignIn/> }/>
+      </Routes>
     </>
   );
 }
--- src/components/SignIn/SignIn.jsx
+++ src/components/SignIn/SignIn.jsx
@@ -1,27 +1,23 @@
 import { Link } from "react-router-dom";
 import { Form, FormButton, FormControl, FormInput, FormLabel, FormTitle, FormWrapper, LoginText } from "./styled";
 
-const SignUp = () => {
+const SignIn = () => {
     return (
         <FormWrapper>
             <Form>
-                <FormTitle>Регистрация</FormTitle>
+                <FormTitle>Вход</FormTitle>
                 <FormControl>
-                    <FormLabel>Имя</FormLabel>
-                    <FormInput></FormInput>
                     <FormLabel>Электронная почта</FormLabel>
                     <FormInput></FormInput>
                     <FormLabel>Пароль</FormLabel>
                     <FormInput type="password"></FormInput>
-                    <FormLabel>Подтвердите пароль</FormLabel>
-                    <FormInput type="password"></FormInput>
                 </FormControl>
-                <FormButton>Зарегистрироваться</FormButton>
+                <FormButton>Войти</FormButton>
             </Form>
-            <LoginText>Уже зарегистрированы? Выполните <Link to="/signin">вход</Link></LoginText>
+            <LoginText>Впервые здесь? <Link to="/signup">Зарегистрируйтесь</Link></LoginText>
         </FormWrapper>
     );
 
 };
 
-export default SignUp;
+export default SignIn;
