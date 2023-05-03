3b68680700c3020a64076945eaa1238db2e2ba7f
Elizabeth <elizaveta.chernosheikina@yandex.ru>
2023-05-03 16:15:25 +0300

    add styles for userCard

--- src/components/Users/User/User.jsx
+++ src/components/Users/User/User.jsx
@@ -1,13 +1,58 @@
-import { Navigate } from "react-router-dom";
-import { useAuth } from "../../../hooks/use-auth";
+import phone from "../../../icons/telephone.svg"
+import email from "../../../icons/envelope.svg"
+import { Header, HeaderBtnLeft, HeaderBtnRight, HeaderText, HeaderTextWrapper, HeaderTitle, UserCarInfo, UserCardContact, UserCardContactWrapper, UserCardText, UserCardWrapper, UserContactIcon, UserPhoto } from "./styled";
+import { useNavigate } from "react-router-dom";
 
 const User = () => {
 
-    const {isAuth} = useAuth();
+    const navigate = useNavigate();
 
-    return isAuth ? (
-        <h1>Partner card</h1>
-    ) : (<Navigate to="/signin" /> )
+    const handleUsers = () => {
+        navigate("/users");
+    }
+    const signOut = () => {
+        navigate("/signIn");
+    }
+
+    return (
+        <>
+            <Header>
+                <HeaderBtnLeft onClick={handleUsers}>Назад</HeaderBtnLeft>
+                <UserCarInfo>
+                    <UserPhoto></UserPhoto>
+                    <HeaderTextWrapper>
+                        <HeaderTitle>Tomas Black</HeaderTitle>
+                        <HeaderText>Партнер</HeaderText>
+                    </HeaderTextWrapper>
+                </UserCarInfo>
+                <HeaderBtnRight onClick={signOut}>Выход</HeaderBtnRight>
+            </Header>
+            <UserCardWrapper>
+                <UserCardText>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </UserCardText>
+                <UserCardContactWrapper>
+                    <UserCardContact>
+                        <UserContactIcon>
+                            <img src={phone} alt="SVG logo" />
+                        </UserContactIcon>
+                        8 (800) 555-35-35
+                    </UserCardContact>
+                    <UserCardContact>
+                        <UserContactIcon>
+                            <img src={email} alt="SVG logo" />
+                        </UserContactIcon>
+                        example@gmail.com
+                    </UserCardContact>
+                </UserCardContactWrapper>
+            </UserCardWrapper>
+        </>
+    )
 }
+
+
 
 export default User;
---
+++ src/components/Users/User/styled.js
@@ -0,0 +1,93 @@
+import styled from "styled-components";
+import img from "../../../icons/cat.jpeg"
+
+const Header = styled.div`
+    background-color: rgba(81, 38, 137, 1);
+    color: rgba(248, 248, 248, 1);
+`;
+
+const HeaderBtnLeft = styled.button`
+    position: absolute;
+    top: 40px;
+    left: 120px;
+    background-color: rgba(81, 38, 137, 1);
+    color: rgba(248, 248, 248, 1);
+    border: 1px solid #F8F8F8;
+    padding: 8px 16px;
+    border-radius: 8px;
+    font-size: 16px;    
+`;
+
+const HeaderTextWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+`;
+
+const UserCarInfo = styled.div`
+    display: flex;   
+    justify-content: flex-start;
+    align-items: stretch;
+    margin-left: 200px;
+`;
+
+const UserPhoto = styled.div`
+    margin: 39px;
+    width: 185px;
+    height: 185px;
+    background-image: url('${img}');
+    background-size: cover;
+    border-radius: 100px;
+`;
+
+const HeaderTitle = styled.p`
+    margin-bottom: 16px;
+    font-size: 64px;
+    line-height: 75px;
+`;
+
+const HeaderText = styled.p`
+    font-size: 20px;
+    line-height: 23px;
+`;
+
+const HeaderBtnRight = styled.button`
+    position: absolute;
+    top: 40px;
+    right: 120px;
+    background-color: rgba(81, 38, 137, 1);
+    color: rgba(248, 248, 248, 1);
+    border: 1px solid #F8F8F8;
+    padding: 8px 16px;
+    border-radius: 8px;
+    font-size: 16px;    
+`;
+
+const UserCardWrapper = styled.div`
+    margin: 49px 80px;
+    display: flex;    
+    justify-content: space-around;
+`;
+
+const UserCardText = styled.p`
+    font-size: 16px;
+    max-width: 630px;
+    line-height: 25px;
+`;
+
+const UserCardContactWrapper = styled.div`
+    width: 200px;
+    font-size: 16px;
+`;
+
+const UserCardContact = styled.p`
+    margin-bottom: 25px;
+`;
+
+const UserContactIcon = styled.span`
+    padding-right: 10px;
+`;
+
+
+export { Header, HeaderBtnLeft, UserCarInfo, UserPhoto, HeaderTextWrapper,
+     HeaderTitle, HeaderText, HeaderBtnRight, UserCardWrapper, UserCardText,
+      UserCardContactWrapper, UserCardContact,UserContactIcon };
